34a63b6cb30f3bd2d011b7ceb28837bf6e962fc5
SchwenderOne <lucasjaiin@gmail.com>
2025-03-31 18:12:28 +0200

    Initial Commit

---
+++ ArchiveApp/ArchiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveAppApp.swift
+//  ArchiveApp
+//
+//  Created by Lucas Schwender Jain on 31.03.25.
+//
+
+import SwiftUI
+
+@main
+struct ArchiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArchiveAppTests/ArchiveAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveAppTests.swift
+//  ArchiveAppTests
+//
+//  Created by Lucas Schwender Jain on 31.03.25.
+//
+
+import Testing
+@testable import ArchiveApp
+
+struct ArchiveAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
